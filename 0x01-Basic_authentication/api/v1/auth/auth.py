a272fbafa5ce516a10bd135680efbb7860197470
CodexRodney <rodney002mwanje@gmail.com>
2022-09-09 11:30:21 +0300

    Documented class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,9 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """
+    defines Auth class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) \
             -> bool:
         """
