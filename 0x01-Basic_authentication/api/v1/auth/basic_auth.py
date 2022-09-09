e8fb87204f41198834a8f2658372c4be26185adb
CodexRodney <rodney002mwanje@gmail.com>
2022-09-10 01:02:35 +0300

    decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Defines a class BasicAuth
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,4 +23,20 @@ class BasicAuth(Auth):
         if authorization_header.find("Basic ") == 0:
             return authorization_header.strip("Basic ")
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base_64 = base64.b64decode(base64_authorization_header)
+            return base_64.decode('utf-8')
+        except Exception:
             return None
