328d475af5f4b60cd59a2e29ddb39eda2be04163
CodexRodney <rodney002mwanje@gmail.com>
2022-09-09 15:15:46 +0300

    Updated method require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,14 @@ class Auth:
         """
         Returns False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for x in excluded_paths:
+            if path.strip('/') == x.strip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> TypeVar('User'):
         """
