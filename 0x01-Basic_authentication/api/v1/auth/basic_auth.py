91caad4acfe849e746d2eb51e9bfa608090781d0
CodexRodney <rodney002mwanje@gmail.com>
2022-09-09 18:45:33 +0300

    Added method extract_base64_authorization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,13 @@ class BasicAuth(Auth):
     """
     a class BasicAuth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.find("Basic ") == 0:
+            return authorization_header.strip("Basic ")
+        else:
+            return None
