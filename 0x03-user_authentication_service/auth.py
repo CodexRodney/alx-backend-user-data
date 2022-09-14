ad61e2693b815d8f61bc0938081590561241a738
CodexRodney <rodney002mwanje@gmail.com>
2022-09-14 19:58:54 +0300

    Added class Auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,11 @@
 Defines a class Auth
 """
 
+from db import DB
 from bcrypt import hashpw, gensalt
+from typing import TypeVar
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +15,30 @@ def _hash_password(password: str) -> bytes:
     Returns the hash password
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes values
+        """
+        self._db = DB()
+
+    def register_user(self, email: str,
+                      password: str) -> TypeVar('User'):
+        """
+        Registers a user
+        """
+        try:
+            (self._db._session.query(User)
+             .filter(User.email == email).one())
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            user = User(email=email, hashed_password=password)
+            self._db._session.add(user)
+            self._db._session.commit()
+        else:
+            raise ValueError(f'User {email} already exists')
