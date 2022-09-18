e746c049901a479b8048a21294eb2a71680e8905
CodexRodney <rodney002mwanje@gmail.com>
2022-09-19 00:16:04 +0300

    Added valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,7 +4,7 @@ Defines a class Auth
 """
 
 from db import DB
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -41,3 +41,17 @@ class Auth:
             return user
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Locates a user by email, if user exists it checks
+        whether the password is the valid and returns True
+        otherwise returns False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        else:
+            return checkpw(password.encode('utf-8'),
+                           user.hashed_password)
