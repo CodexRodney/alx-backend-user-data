42dde3841270dfda6a7680df3526a977f8927f9b
CodexRodney <rodney002mwanje@gmail.com>
2022-09-18 23:58:14 +0300

    Added users view function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,15 +3,33 @@
 Flask app
 """
 
+from auth import Auth
 from flask import Flask, jsonify
+from flask import request
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def hello():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users() -> None:
+    """
+    Registers a user
+    """
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
